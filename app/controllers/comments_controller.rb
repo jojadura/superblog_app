07013bb0746cadff2e2d52d9df9816b713a64c34
John Jairo <jojadura@hotmail.com>
2016-03-01 16:05:04 -0500

    Persolizando Devise

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,25 +2,18 @@ class CommentsController < ApplicationController
     
 
 def create
-    p "Hola"
-    p params[:post_id]
-    p params[:user_id]
-    post = Post.find(params[:post_id])  #rake routes muestra /posts/:post_id/comments, de ahi sale :post_id, que es el valor que va por la URL.    
-    user = User.find(params[:user_id])     
-    p post
-    p user
-    exit
-    @comment = post.comments.new(comment_params)
+    @post = Post.find(params[:post_id])  #rake routes muestra /posts/:post_id/comments, de ahi sale :post_id, que es el valor que va por la URL.    
+    @comment = @post.comments.new(comment_params)
     if @comment.save      
-        redirect_to post
+        redirect_to @post
     else
-      @comment = Post.find(params[:post_id])
+      @post = Post.find(params[:post_id])
       render "posts/show"
     end   
 end
 
   private 
     def comment_params
-      params.require(:comment).permit(:body)
+      params.require(:comment).permit(:body,:user_id)
     end
 end
